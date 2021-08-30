2b1d0fb148a8e2ed26c0b54430b65c9a6f844d1c
Pushkaraj <pushkarajpalnitkar@gmail.com>
2021-08-30 10:47:41 -0400

    App with scatter plot with bounds and added functionality of the filter and sorted table

--- src/app.py
+++ src/app.py
@@ -1,75 +1,89 @@
 import config
-
-import pickle
 import dash
-import numpy as np
-import pandas as pd
 import plotly.express as px
-import dash_core_components as dcc
-import dash_html_components as html
+import numpy as np
 
 from dash.dependencies import Input
 from dash.dependencies import Output
+from dash.dependencies import State
+from dash.dependencies import ALL
 
+from read import read
+from layout import create_new_dropdown_div
+from layout import create_layout
+from layout import create_table_df
+from layout import thresholds
 
-def main(schema_file, data_file):
+def main(dataframe):
+    app.layout = create_layout(dataframe.columns)
 
-    def read(schema_file, data_file):
-        with open(schema_file, "rb") as file:
-            # List of 39 dictionaries each with keys name, type, baseType
-            schema = pickle.load(file)
-        with open(data_file, "rb") as file:
-            # List of lists with shape 1987 x 39
-            data = pickle.load(file)
-            data = np.array(data)
-
-        schema_names = list(map(lambda dictionary: dictionary["name"], schema))
-        
-        content = {}
-        for ind, name in enumerate(schema_names):
-            content[name] = list(data[:, ind])
-
-        return pd.DataFrame(content)
-
-    def create_layout(df):
-
-        return html.Div(
-            children=[
-                html.H1(children = "Uncertainty Managed Access to Microstructures Interface Project",
-                style={'textAlign': 'center'}),
-                html.Div([
-                    html.Div([dcc.Dropdown(
-                        id = "xaxis-column",
-                        options = [{'label': col, 'value': col} for col in df.columns[2:]],
-                        value = "ABS_wf_D",
-                    )]),
-                    html.Div([dcc.Dropdown(
-                        id = "yaxis-column",
-                        options = [{'label': col, 'value': col} for col in df.columns[2:]],
-                        value = "STAT_CC_D",
-                    )])
-                ]),
-                dcc.Graph(id='indicator-graphic')
-            ])
-
-    #####################################################################
-    dataframe = read(schema_file, data_file)
-
-    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-    app = dash.Dash("__main__", external_stylesheets=external_stylesheets)
-    app.layout = create_layout(dataframe)
-
-    #####################################################################
-
+    ############################## Update Scatter Plot ##############################
     @app.callback(
         Output("indicator-graphic", "figure"),
         Input("xaxis-column", "value"),
-        Input("yaxis-column", "value"))
-    def update_output(xaxis_column_name, yaxis_column_name):
-        fig = px.scatter(dataframe, x=xaxis_column_name, y=yaxis_column_name)
+        Input("lbx", "value"),
+        Input("ubx", "value"),
+        Input("yaxis-column", "value"),
+        Input("lby", "value"),
+        Input("uby", "value")
+    )
+    def update_output(
+        xaxis_column_name,
+        lbx,
+        ubx,
+        yaxis_column_name,
+        lby,
+        uby):
+
+        lbx, lby = list(map(lambda val: -1000 if val==None else val, (lbx, lby)))
+        ubx, uby = list(map(lambda val: 1000 if val==None else val, (ubx, uby)))
+
+        upadted_dataframe = thresholds(
+            dataframe,
+            xaxis_column_name,
+            yaxis_column_name,
+            lbx,
+            ubx,
+            lby,
+            uby
+        )
+
+        fig = px.scatter(upadted_dataframe, x=xaxis_column_name, y=yaxis_column_name)
         return fig
 
-    app.run_server(debug=False)
+
+    ############################## Update Dropdown ##############################
+    @app.callback(
+        Output("dropdown-container", "children"),
+        Input("add-filter", "n_clicks"),
+        State("dropdown-container", "children")
+    )
+    def display_dropdown(button_click, dropdown_children_state):
+        new_dropdown_div = create_new_dropdown_div(button_click, dataframe.columns[2:])
+        dropdown_children_state.append(new_dropdown_div)
+
+        return dropdown_children_state
+
+    @app.callback(
+        Output("table-id", "data"),
+        Input("add-filter", "n_clicks"),
+        State({'type': 'filter-dropdown', 'index': ALL}, 'value'),
+        State({'type': 'number', 'index': ALL}, 'value')
+    )
+    def on_click(number_of_times_button_has_clicked, filter_dropdown_state, filter_limits_state):
+        selected_features = filter_dropdown_state
+        selected_bounds = np.array(filter_limits_state).reshape(-1, 2)
+        table_df = create_table_df(
+            dataframe,
+            number_of_times_button_has_clicked,
+            selected_features,
+            selected_bounds
+        )
+        return table_df.to_dict("records")
+
+    app.run_server(debug=True)
 
 if __name__ == "__main__":
-    main(config.SCHEMA_FILE, config.DATA_FILE)
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = dash.Dash("__main__", external_stylesheets=external_stylesheets)
+    main(read(config.SCHEMA_FILE, config.DATA_FILE))
---
+++ src/layout.py
@@ -0,0 +1,170 @@
+import dash_table
+
+import pandas as pd
+import dash_html_components as html
+import dash_core_components as dcc
+
+def thresholds(df, x, y, lb_x, ub_x, lb_y, ub_y):
+    """Returns updated dataframe after thresholding for scatter plot"""
+    new_df = df[(df[x]>=lb_x) & (df[x]<=ub_x) & (df[y]>=lb_y) & (df[y]<=ub_y)]
+    return new_df
+
+def create_input(input_id, input_placeholder):
+    """Return input object to receive bounds"""
+    return dcc.Input(
+                id=input_id,
+                type="number",
+                placeholder=input_placeholder,
+                min=-1001,
+                max=1001,
+                style={"width":"30%"}
+            )
+
+def create_dropdown_div(input_id, default_value, lb, ub, dropdown_list):
+    """Return Div object with dropdown menue along with the options of receiving lower bound
+    and upper bound for adding filter"""
+    return html.Div(
+            children=[
+                        dcc.Dropdown(
+                                    id=input_id,
+                                    options=[{'label': col, 'value': col} for col in dropdown_list],
+                                    value=default_value ,
+                                    style={"width":"60%"}                       
+                        ),
+                        lb,
+                        ub,            
+            ],
+            style = {"display":"inline-flex"}
+        )
+
+def create_new_dropdown_div(click_event, dropdown_list):
+    """Returns dropdown object indexed according to click event (number of clicks)
+    for adding new filter when one is selected"""
+    new_dropdown = dcc.Dropdown(
+            id={
+                "type": "filter-dropdown",
+                "index": click_event
+            },
+            options=[{"label":i, "value":i} for i in dropdown_list],
+            style={"width":"60%"}
+        )
+
+    new_lb = create_input({"type":"number", "index": click_event}, "Lower Bound")
+    new_ub = create_input({"type":"number", "index": click_event}, "Upper Bound")
+
+    return html.Div(
+            [
+                    html.Div(
+                        [
+                            new_dropdown,
+                            new_lb,
+                            new_ub
+                        ],
+                        style = {"display":"inline-flex"}
+                )
+            ],
+            style={"display":"grid"}
+        )
+
+def create_table_df(dataframe, clicks, features, bounds):
+    """Returns pandas dataframe of feature names and number of out of bounds values"""
+    dataframe = dataframe.drop(["case_name", "source"], axis=1)
+    table_df = pd.DataFrame({
+        "Features": dataframe.columns,
+        "Out-of-range-values": [0]*len(dataframe.columns)
+    })
+    if clicks>0:
+        data = dict(zip(table_df["Features"].values, table_df["Out-of-range-values"].values))
+        for ind, feat in enumerate(features):
+            lb, ub = bounds[ind]
+            data[feat] = dataframe[(dataframe[feat]<lb) | (dataframe[feat]>ub)].shape[0]
+
+        table_df = pd.DataFrame({
+            "Features": list(data.keys()),
+            "Out-of-range-values": list(data.values())
+        })
+
+    table_df.sort_values("Out-of-range-values", inplace=True, ascending=False)
+    return table_df
+
+def create_layout(features):
+    """
+    Returns page layout
+    """
+
+    ################## Page Header ##################
+    header_div = html.Div(
+        children=[
+            html.H1(
+                children="Uncertainty Managed Access to Microstructures Interface Project",
+                style={'textAlign': 'center'}
+            ),
+        ]
+    )
+
+    ################## Scatter Plot ##################
+    lb_x = create_input("lbx", "Lower Bound")
+    lb_y = create_input("lby", "Lower Bound")
+    ub_x = create_input("ubx", "Upper Bound")
+    ub_y = create_input("uby", "Upper Bound")
+
+    x_dropdown_div = create_dropdown_div("xaxis-column","ABS_wf_D", lb_x, ub_x, features[2:])
+    y_dropdown_div = create_dropdown_div("yaxis-column", "STAT_CC_D", lb_y, ub_y, features[2:])
+
+    dropdown_div = html.Div(
+        children=[
+            x_dropdown_div,
+            y_dropdown_div
+        ],
+        style={"display":"grid"}
+    )
+
+    graph_object = dcc.Graph(id='indicator-graphic')
+
+    scatter_plot_div = html.Div(
+        children=[
+            html.H2(
+                children = "Scatter Plot"
+            ),
+            dropdown_div,
+            graph_object
+        ]
+    )
+
+    ################## Adding Filters ##################
+    filter_div = html.Div(
+        children=[
+            html.H2(
+                children="Filters"
+            ),
+            html.P(
+                children="To remove filter click add filter with bounds -1000 and 1000."
+            ),
+            html.Button("Add Filter", id="add-filter", n_clicks=0),
+            html.Div(id="dropdown-container", children=[]),
+            html.Div(id="empty-output")
+        ]
+    )
+    ################## Table Object ##################
+    table_object = dash_table.DataTable(
+        id="table-id",
+        columns = [{"name": i, "id": i} for i in ["Features", "Out-of-range-values"]]
+    )
+    
+    table_div = html.Div(
+            children=[
+                html.H2(
+                    children="Table"
+                ),
+                table_object      
+            ]
+        )
+
+    return html.Div(
+        children=[
+            header_div,
+            scatter_plot_div,
+            filter_div,
+            table_div
+        ]
+    )
